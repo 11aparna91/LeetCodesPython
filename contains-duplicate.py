05209e8a663d5a1fa259053a62f6a499a78a2d74
Aparna Popade <popadep@gmail.com>
2022-02-07 12:29:27 -0700

    Update contains-duplicate.py

--- contains-duplicate.py
+++ contains-duplicate.py
@@ -1,6 +1,8 @@
 #################### Problem number: 217 ##########################
 
-
+################## Using Hash Table ###############################
+############# Time Complexity=O(n) ################################
+################# Space Complexity=O(n) ###########################
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         dict2={}
@@ -14,4 +16,15 @@ class Solution:
         
         return False
       
-##############################################
+############################################## Solution 2#############
+######################################## Using Sorting ###############
+########################### Time Complexity=O(nlogn) (WORST CASE COMPLEXITY OF SORTING)########
+########################### Space Complexity=O(1) ####################
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return True
+        return False
