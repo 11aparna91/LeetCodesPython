c1d897dfa8f44a0a603efc2e33608dc27387cf1a
Aparna Popade <popadep@gmail.com>
2022-06-17 07:23:22 -0700

    Create rotate-string.py
    Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
    
    A shift on s consists of moving the leftmost character of s to the rightmost position.
    
    For example, if s = "abcde", then it will be "bcdea" after one shift.
     
    
    Example 1:
    
    Input: s = "abcde", goal = "cdeab"
    Output: true
    Example 2:
    
    Input: s = "abcde", goal = "abced"
    Output: false
     
    
    Constraints:
    
    1 <= s.length, goal.length <= 100
    s and goal consist of lowercase English letters.

---
+++ rotate-string.py
@@ -0,0 +1,8 @@
+########### Problem Number: 796 #####################
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        for i in range(len(s)):
+            if s[i:]+s[0:i] == goal:
+                return True
+        
+        return False
