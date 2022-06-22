2a172a79ffd6700e2400020f37be1a4b08b638b6
Aparna Popade <popadep@gmail.com>
2022-06-22 01:53:56 -0700

    Update intersection-of-two-arrays-ii.py

--- intersection-of-two-arrays-ii.py
+++ intersection-of-two-arrays-ii.py
@@ -17,14 +17,12 @@ class Solution:
         two=0
         ans=[]
         
-        while one < len(nums1) and two < len(nums2):
-            
+        while one < len(nums1) and two < len(nums2):   
             if nums1[one] < nums2[two]:
                 one+=1
             elif nums2[two] < nums1[one]:
                 two+=1
             else:
-                
                 ans.append(nums1[one])
                 one+=1
                 two+=1
