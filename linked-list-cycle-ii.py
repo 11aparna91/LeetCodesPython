c76d3a89cadfdc1049fe3777e67539cb468eca5f
Aparna Popade <popadep@gmail.com>
2022-08-06 11:26:33 -0700

    Create linked-list-cycle-ii.py
    Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
    
    There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
    
    Do not modify the linked list.
    
     
    
    Example 1:
    
    
    Input: head = [3,2,0,-4], pos = 1
    Output: tail connects to node index 1
    Explanation: There is a cycle in the linked list, where tail connects to the second node.
    Example 2:
    
    
    Input: head = [1,2], pos = 0
    Output: tail connects to node index 0
    Explanation: There is a cycle in the linked list, where tail connects to the first node.
    Example 3:
    
    
    Input: head = [1], pos = -1
    Output: no cycle
    Explanation: There is no cycle in the linked list.
     
    
    Constraints:
    
    The number of the nodes in the list is in the range [0, 104].
    -105 <= Node.val <= 105
    pos is -1 or a valid index in the linked-list.
     
    
    Follow up: Can you solve it using O(1) (i.e. constant) memory?

---
+++ linked-list-cycle-ii.py
@@ -0,0 +1,18 @@
+##### Time Complexity=O(N) ###########
+##### Space Complexity=O(1) ##########
+## Problem Number: 142 ##############
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution(object):
+    def detectCycle(self, head):
+        slow = fast = head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+            if slow == fast: break
+        else: return None  # if not (fast and fast.next): return None
+        while head != slow:
+            head, slow = head.next, slow.next
+        return head
