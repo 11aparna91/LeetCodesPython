5932e94fa8ac4d3bb6e45b79c89fbc3662f8822a
Aparna Popade <popadep@gmail.com>
2022-08-13 13:06:36 -0700

    Update search-insert-position.py

--- search-insert-position.py
+++ search-insert-position.py
@@ -5,3 +5,20 @@ class Solution:
             if nums[i] >= target:
                 return i
         return len(nums)
+
+########## Time complexity=O(log n) ############
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l=0
+        r=len(nums)-1
+        
+        while l<=r:
+            mid=(l+r)//2
+            if nums[mid] == target:
+                return mid
+            else:
+                if nums[mid]<target:
+                    l=mid+1
+                else:
+                    r=mid-1
+        return l
