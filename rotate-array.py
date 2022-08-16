e1857541de1d26e020390b54d33d0e2be35f82af
Aparna Popade <popadep@gmail.com>
2022-08-16 15:52:37 -0700

    Update rotate-array.py

--- rotate-array.py
+++ rotate-array.py
@@ -11,3 +11,25 @@ class Solution:
         
         for i in range(len(nums)):
             nums[i]=res[i]
+            
+            
+##############################################
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k= k % len(nums)
+        self.reverse(nums, 0, len(nums)-1)
+        self.reverse(nums, 0, k-1)
+        self.reverse(nums, k, len(nums)-1)
+    
+    def reverse(self, nums,l,r):
+            while l<r:
+                nums[l], nums[r] =  nums[r], nums[l]
+                l,r= l+1, r-1
+            
+
+            
