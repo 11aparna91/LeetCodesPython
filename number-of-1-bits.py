30c49bbb7c718e633afb235467e4934db1ed34b5
Aparna Popade <popadep@gmail.com>
2022-08-24 11:00:56 -0700

    Update number-of-1-bits.py

--- number-of-1-bits.py
+++ number-of-1-bits.py
@@ -4,6 +4,17 @@ class Solution:
         count=0
         while n:
             count+=1 
-            n &=n-1   # AND with the 1's complement
+            n &=n-1   # AND with the 1's complement to cancel the rightmost 1
         return count
             
+#######################
+
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res=0
+        while n:
+            res += n%2 #to get the last bit
+            n = n>>1   # right shift
+        return res
+        
