dcd736ca0d6f3e6151b7ffed52a3233c0ffe325d
Aparna Popade <popadep@gmail.com>
2022-08-25 13:30:40 -0700

    Update reverse-bits.py

--- reverse-bits.py
+++ reverse-bits.py
@@ -3,8 +3,8 @@ class Solution:
     def reverseBits(self, n: int) -> int:
         reverse=0
         for i in range(32):
-            bit=(n>>i) & 1
-            reverse= reverse | ( bit<< (31-i))
+            bit=(n>>i) & 1  #(logical AND of ith bit with 1 to get ith bit)
+            reverse= reverse | ( bit<< (31-i))  #(result is from 31st place first to 0th later) 
         return reverse
         
         
