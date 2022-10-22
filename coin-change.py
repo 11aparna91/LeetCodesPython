22b2ff984ae1423a6c6d86706d7f81bc6883a117
Aparna Popade <popadep@gmail.com>
2022-10-21 18:44:33 -0700

    Update coin-change.py
    You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
    
    Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
    
    You may assume that you have an infinite number of each kind of coin.
    
     
    
    Example 1:
    
    Input: coins = [1,2,5], amount = 11
    Output: 3
    Explanation: 11 = 5 + 5 + 1
    Example 2:
    
    Input: coins = [2], amount = 3
    Output: -1
    Example 3:
    
    Input: coins = [1], amount = 0
    Output: 0
     
    
    Constraints:
    
    1 <= coins.length <= 12
    1 <= coins[i] <= 231 - 1
    0 <= amount <= 104

--- coin-change.py
+++ coin-change.py
@@ -1,4 +1,5 @@
 ### Problem Number 322 #########
+## Time complexity O(S*n)
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         dp= [amount+1] * (amount+1)
@@ -12,3 +13,4 @@ class Solution:
                     dp[i]= min(dp[i], 1 + dp[i-c])
         return dp[amount] if dp[amount]!=(amount + 1) else -1
     
+   
